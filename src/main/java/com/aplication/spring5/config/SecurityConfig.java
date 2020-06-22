1d74d62438b33a27a47ee8ac337232cbe8a77753
project2591 <sak.25.deep@gmail.com>
2020-06-22 13:39:20 +0530

    my first project setup with spring security

---
+++ src/main/java/com/aplication/spring5/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.aplication.spring5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		//add users for in memory authentication
+		
+		UserBuilder users=User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE");
+		auth.inMemoryAuthentication().withUser("mary").password("{noop}test123").roles("MANAGER");
+		auth.inMemoryAuthentication().withUser("adam").password("{noop}test123").roles("ADMIN");
+		
+	}
+
+}
---
+++ src/main/java/com/aplication/spring5/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.aplication.spring5.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
