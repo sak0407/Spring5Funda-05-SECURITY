be513257429451d548adc51f0a1eeadd2f1076a4
project2591 <sak.25.deep@gmail.com>
2020-06-22 15:20:09 +0530

    login and logout form

--- src/main/java/com/aplication/spring5/config/SecurityConfig.java
+++ src/main/java/com/aplication/spring5/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.aplication.spring5.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,8 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		//add users for in memory authentication
-		
+		//add users for in memory authentication		
 		UserBuilder users=User.withDefaultPasswordEncoder();
 		
 		auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE");
@@ -23,5 +23,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth.inMemoryAuthentication().withUser("adam").password("{noop}test123").roles("ADMIN");
 		
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/loginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+			.and()
+				.logout()
+				.permitAll();
+				
+				
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/webapp/WEB-INF/view/home.jsp
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -1,5 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +10,13 @@
 </head>
 <body>
 		<h2>Welcome !!</h2>
+		<br>
+		<!--  Add a logout -->
+		
+		<form:form action="${pageContext.request.contextPath}/logout" 
+							   method="POST" >
+							   
+		 <input type="submit" value="Logout" />					   
+		</form:form>
 </body>
 </html>
