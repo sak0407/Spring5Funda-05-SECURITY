8f1c730d24bffcdce53234e2315cfb90c5f1583c
project2591 <sak.25.deep@gmail.com>
2020-06-24 23:02:26 +0530

    jdbc database authentication

--- src/main/java/com/aplication/spring5/config/SecurityConfig.java
+++ src/main/java/com/aplication/spring5/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.aplication.spring5.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,15 +15,24 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private DataSource securityDataSource;
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		//add users for in memory authentication		
-		UserBuilder users=User.withDefaultPasswordEncoder();
+		//UserBuilder users=User.withDefaultPasswordEncoder();
 		
-		auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE");
-		auth.inMemoryAuthentication().withUser("mary").password("{noop}test123").roles("EMPLOYEE","MANAGER");
-		auth.inMemoryAuthentication().withUser("adam").password("{noop}test123").roles("EMPLOYEE","ADMIN");
+		//auth.inMemoryAuthentication().withUser("john").password("{noop}test123").roles("EMPLOYEE");
+		//auth.inMemoryAuthentication().withUser("mary").password("{noop}test123").roles("EMPLOYEE","MANAGER");
+		//auth.inMemoryAuthentication().withUser("adam").password("{noop}test123").roles("EMPLOYEE","ADMIN");
+		
+		
+		//use jdbc authenticvation
+		
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
 	}
 	
